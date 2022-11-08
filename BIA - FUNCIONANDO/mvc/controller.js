2130b1ed88536fbe06a9d0071a3bcd3165bb7980
Ana Beatriz Castro <113535073+beatrizprog@users.noreply.github.com>
2022-11-08 11:34:33 -0300

    novos arquivos adicionados

---
+++ BIA - FUNCIONANDO/mvc/controller.js
@@ -0,0 +1,24 @@
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('rua').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('cidade').value=("");
+    document.getElementById('uf').value=("");
+   
+  }
+  function meu_callback(conteudo) {
+  if (!("erro" in conteudo)) {
+    //Atualiza os campos com os valores.
+    document.getElementById('rua').value=(conteudo.logradouro);
+    document.getElementById('bairro').value=(conteudo.bairro);
+    document.getElementById('cidade').value=(conteudo.localidade);
+    document.getElementById('uf').value=(conteudo.uf);
+    
+  } //end if.
+  else {
+    //CEP não Encontrado.
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+  }
+  }
+  
