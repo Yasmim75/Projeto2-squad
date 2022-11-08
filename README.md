7417dd307cd53c7194532b99bb8607d7ed4da749
Ana Beatriz Castro <113535073+beatrizprog@users.noreply.github.com>
2022-11-08 11:17:43 -0300

    novos arquivos enviados.

---
+++ README.md
@@ -0,0 +1,6 @@
+# PROJETO EM GRUPO MÓDULO 2
+
+ 
+
+
+Link para página: https://yasmim75.github.io/Projeto2-squad/
---
+++ form.js
@@ -0,0 +1,160 @@
+function limpa_formulário_cep() {
+  //Limpa valores do formulário de cep.
+  document.getElementById('rua').value=("");
+  document.getElementById('bairro').value=("");
+  document.getElementById('cidade').value=("");
+  document.getElementById('uf').value=("");
+ 
+}
+function meu_callback(conteudo) {
+if (!("erro" in conteudo)) {
+  //Atualiza os campos com os valores.
+  document.getElementById('rua').value=(conteudo.logradouro);
+  document.getElementById('bairro').value=(conteudo.bairro);
+  document.getElementById('cidade').value=(conteudo.localidade);
+  document.getElementById('uf').value=(conteudo.uf);
+  
+} //end if.
+else {
+  //CEP não Encontrado.
+  limpa_formulário_cep();
+  alert("CEP não encontrado.");
+}
+}
+
+function pesquisacep(valor) {
+
+//Nova variável "cep" somente com dígitos.
+var cep = valor.replace(/\D/g, '');
+
+//Verifica se campo cep possui valor informado.
+if (cep != "") {
+
+  //Expressão regular para validar o CEP.
+  var validacep = /^[0-9]{8}$/;
+
+  //Valida o formato do CEP.
+  if(validacep.test(cep)) {
+
+      //Preenche os campos com "..." enquanto consulta webservice.
+      document.getElementById('rua').value="...";
+      document.getElementById('bairro').value="...";
+      document.getElementById('cidade').value="...";
+      document.getElementById('uf').value="...";
+    
+
+      //Cria um elemento javascript.
+      var script = document.createElement('script');
+
+      //Sincroniza com o callback.
+      script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+      //Insere script no documento e carrega o conteúdo.
+      document.body.appendChild(script);
+
+  } //end if.
+  else {
+      //cep é inválido.
+      limpa_formulário_cep();
+      alert("Formato de CEP inválido.");
+  }
+} //end if.
+else {
+  //cep sem valor, limpa formulário.
+  limpa_formulário_cep();
+}
+};
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+const RG = document.getElementById("RG");
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const passwordConfirmationValue = passwordConfirmation.value;
+  const RGValue = RG.value;
+  
+
+  if (usernameValue === "") {
+    setErrorFor(username, "O nome de usuário é obrigatório.");
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "O email é obrigatório.");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido.");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "A senha é obrigatória.");
+  } else if (passwordValue.length < 7) {
+    setErrorFor(password, "A senha precisa ter no mínimo 7 caracteres.");
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (passwordConfirmationValue === "") {
+    setErrorFor(passwordConfirmation, "A confirmação de senha é obrigatória.");
+  } else if (passwordConfirmationValue !== passwordValue) {
+    setErrorFor(passwordConfirmation, "As senhas não conferem.");
+  } else {
+    setSuccessFor(passwordConfirmation);
+  }
+  if (RGValue === "") {
+    setErrorFor(RG, "RG é obrigatório.");
+  } 
+  else {
+    setSuccessFor(RG);
+  }
+  
+  const formControls = form.querySelectorAll(".form-control");
+
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success";
+  });
+
+  if (formIsValid) {
+    console.log("O formulário está 100% válido!");
+  }
+  
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  // Adiciona a mensagem de erro
+  small.innerText = message;
+
+  // Adiciona a classe de erro
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  // Adicionar a classe de sucesso
+  formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
